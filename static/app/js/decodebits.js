85a770c84c775bbdcf7c22b882f4159714180032
sj <stroudafk@gmail.com>
2021-12-04 14:51:46 -0600

    created mock test interface with file drag & drop and file selection. added cascading style sheets. added favicon

--- static/app/js/decodebits.js
+++ static/app/js/decodebits.js
@@ -3,7 +3,21 @@
 //  document.getElementById("btn").onclick = test();  
   //var x = 
   //document.getElementById('btn').onclick = test()
-  function test(){
+
+const input = document.querySelector('input');
+const preview = document.querySelector('.preview');
+
+input.style.opacity = 0;
+
+input.addEventListener('change', validateInput);
+function validateInput(){
+  alert('Received file');
+}
+
+
+
+
+function test(){
   console.log("testing");
   }
   var goBtn = document.getElementById("btn");
@@ -91,4 +105,31 @@
   function decodeAddress(addressBits){
     return parseInt(addressBits, 16)
   }
+function dropHandler(ev) {
+  console.log('File(s) dropped');
 
+  // Prevent default behavior (Prevent file from being opened)
+ ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+      // If dropped items aren't files, reject them
+      if (ev.dataTransfer.items[i].kind === 'file') {
+        var file = ev.dataTransfer.items[i].getAsFile();
+        console.log('... file[' + i + '].name = ' + file.name);
+      }
+    }
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+      console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+    }
+  }
+}
+function dragOverHandler(ev) {
+  console.log('File(s) in drop zone');
+
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+}
